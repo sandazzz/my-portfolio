d02930460eb5821e77168968bb3965a6f7babf02
sandazzz <rsandarisoa@gmail.com>
2024-01-04 00:37:48 +0700

    Update

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,76 +1,52 @@
 <script setup></script>
 
 <template>
-  <div class="navbar bg-base-100 fixed z-10 text-violet-800">
-    <div class="navbar-start">
-      <div class="dropdown">
-        <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-5 w-5"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
+  <header
+    class="flex justify-center w-full min-h-16 p-2 bg-base-100 fixed z-10 text-violet-800"
+  >
+    <div class="flex justify-between w-4/6">
+      <div class="">
+        <div class="dropdown">
+          <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M4 6h16M4 12h16M4 18h7"
+              />
+            </svg>
+          </div>
+          <ul
+            tabindex="0"
+            class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
           >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M4 6h16M4 12h16M4 18h7"
-            />
-          </svg>
+            <li><router-link :to="{ name: 'home' }">Home</router-link></li>
+            <li>
+              <a href="https://github.com/sandazzz/" target="_blank"
+                >My GitHub</a
+              >
+            </li>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/sandarisoa-rakotovelo-698b90255/"
+                target="_blank"
+                >My Linked-in</a
+              >
+            </li>
+            <li><a href="mailto:rsandarisoa@gmail.com">E-mail</a></li>
+          </ul>
         </div>
-        <ul
-          tabindex="0"
-          class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-        >
-          <li><router-link :to="{ name: 'home' }">Home</router-link></li>
-          <li><a href="https://github.com/sandazzz/" target="_blank">My GitHub</a></li>
-          <li><a href="https://www.linkedin.com/in/sandarisoa-rakotovelo-698b90255/" target="_blank">My Linked-in</a></li>
-          <li><a href="mailto:rsandarisoa@gmail.com">E-mail</a></li>
-
-        </ul>
+      </div>
+      <div class="">
+        <a class="btn text-xl">Download my CV</a>
       </div>
     </div>
-    <div class="navbar-center">
-      <a class="btn btn-ghost text-xl">My Site</a>
-    </div>
-    <div class="navbar-end">
-      <button class="btn btn-ghost btn-circle">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-5 w-5"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-          />
-        </svg>
-      </button>
-      <button class="btn btn-ghost btn-circle">
-        <div class="indicator">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-5 w-5"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-            />
-          </svg>
-          <span class="badge badge-xs badge-primary indicator-item"></span>
-        </div>
-      </button>
-    </div>
-  </div>
+  </header>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,20 +8,28 @@
     >
       <div class="hero-overlay bg-[#C2E5FF] bg-opacity-60"></div>
       <div class="hero-content flex-col text-center text-violet-900">
-        <h1 class="mb-3 text-5xl font-bold">Welcome on my Portfolio</h1>
+        <h1 class="mb-3 text-5xl font-bold text-left">
+          Welcome on my Portfolio
+        </h1>
 
-        <div class="flex flex-col items-end">
-          <h2 class="mb-5 text-3xl font-bold">My name is Sanda Rakotovelo</h2>
-          <p class="mb-5 text-xl font-bold">
-            I'am a student in computer science, I like web-development
+        <div
+          class="flex flex-col items-end bg-white bg-opacity-10 rounded-3xl backdrop-blur-md p-4 shadow-2xl"
+        >
+          <h2 class="mb-5 text-3xl font-bold text-left">
+            My name is Sanda Rakotovelo
+          </h2>
+          <p class="mb-5 text-xl font-bold text-right">
+            I'm a student in computer science, I like web-development
           </p>
           <div class="flex flex-row gap-2">
-            <button class="btn btn-active btn-accent text-gray-50">Mail</button>
-            <button class="btn btn-active btn-accent text-gray-50">
-              Github
+            <button class="btn btn-active btn-accent text-gray-50 shadow-md">
+              <a href="mailto:rsandarisoa@gmail.com" target="_blank">Mail</a>
             </button>
-            <button class="btn btn-active btn-accent text-gray-50">
-              Linked-in
+            <button class="btn btn-active btn-accent text-gray-50 shadow-md">
+              <a href="https://github.com/sandazzz/" target="_blank">Github</a>
+            </button>
+            <button class="btn btn-active btn-accent text-gray-50 shadow-md">
+              <a href="https://www.linkedin.com/in/sandarisoa-rakotovelo-698b90255/" target="_blank">Linked-in</a>
             </button>
           </div>
         </div>
@@ -32,18 +40,17 @@
   <div class="flex flex-col items-center">
     <div
       class="hero min-h-screen rotate-180"
-      style="
-        background-image: url(img/Honk-kong.jpg);
-      "
+      style="background-image: url(img/Honk-kong.jpg)"
     >
       <div class="hero-overlay bg-[#C2E5FF] bg-opacity-60"></div>
       <div class="hero-content flex-col w-full text-violet-900 rotate-180">
-
         <h2 class="mb-5 text-4xl font-bold">My Skills</h2>
 
         <div class="flex flex-row justify-around w-full">
-          <div class="flex flex-col items-center w-1/3">
-            <h3 class="mb-5 text-2xl font-bold text-center">Back-end</h3>
+          <div
+            class="flex flex-col items-center w-[320px] bg-white bg-opacity-10 rounded-3xl backdrop-blur-md p-4 shadow-2xl"
+          >
+            <h3 class="mb-5 text-2xl font-bold text-center">Back-End</h3>
             <ul>
               <li>PHP</li>
               <li>Node</li>
@@ -51,15 +58,19 @@
               <li>SQL</li>
             </ul>
           </div>
-          <div class="flex flex-col items-center w-1/3">
-            <h3 class="mb-5 text-2xl font-bold text-center">Front-end</h3>
+          <div
+            class="flex flex-col items-center w-[320px] bg-white bg-opacity-10 rounded-3xl backdrop-blur-md p-4 shadow-2xl"
+          >
+            <h3 class="mb-5 text-2xl font-bold text-center">Front-End</h3>
             <ul>
               <li>Next.Js</li>
               <li>Vue.Js</li>
               <li>Tailwind</li>
             </ul>
           </div>
-          <div class="flex flex-col items-center w-1/3">
+          <div
+            class="flex flex-col items-center w-[320px] bg-white bg-opacity-5 rounded-3xl backdrop-blur-md p-4 shadow-2xl"
+          >
             <h3 class="mb-5 text-2xl font-bold text-center">Others Skills</h3>
             <ul>
               <li>Git</li>
@@ -67,12 +78,28 @@
             </ul>
           </div>
         </div>
-
+        <h2 class="mb-5 text-4xl font-bold">Projects</h2>
         <div class="flex flex-row justify-around w-full">
+          <div
+            class="flex flex-col items-center w-[320px] bg-white bg-opacity-10 rounded-3xl backdrop-blur-md p-4 shadow-2xl"
+          >
+            <h3 class="mb-5 text-2xl font-bold text-center">Strix</h3>
 
+          </div>
+          <div
+            class="flex flex-col items-center w-[320px] bg-white bg-opacity-10 rounded-3xl backdrop-blur-md p-4 shadow-2xl"
+          >
+            <h3 class="mb-5 text-2xl font-bold text-center">Bourse</h3>
+
+          </div>
+          <div
+            class="flex flex-col items-center w-[320px] bg-white bg-opacity-5 rounded-3xl backdrop-blur-md p-4 shadow-2xl"
+          >
+            <h3 class="mb-5 text-2xl font-bold text-center">Pj-3</h3>
+
+          </div>
         </div>
       </div>
     </div>
   </div>
-  
 </template>
