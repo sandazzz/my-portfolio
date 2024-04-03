0fdd58eb6eff829168a46c862498baf498f5a9a4
sandazzz <rsandarisoa@gmail.com>
2024-04-03 14:04:28 +0200

    Update Header.tsx

--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -1,5 +1,7 @@
 export default function Header(){
     return(
-        <header className="sticky top-0 w-full h-16 px-4 bg-white opacity-40"></header>
+        <header className="sticky top-0 w-full h-16 px-4 bg-white opacity-40">
+            hey
+        </header>
     )
 }
